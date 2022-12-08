1f1584c3df45e1bff8ce10cdfbe790b9a2094102
diln20 <andreytorres418@gmail.com>
2022-12-08 18:12:51 -0500

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks=[
+    {
+        id:0,
+        title:'Doctors Appointment',
+        description: 'Hospital'
+    },
+    {
+        id:1,
+        title:'Meeting at School',
+        description: 'School'
+    },
+    {
+        id:2,
+        title:'Food Shopping',
+        description: 'Grocery Store'
+    }
+]
